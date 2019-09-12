61677ea30253cdfcdc3ba43b83d7cc0e35672cf7
nataliamedinat <nataliamedinabotero@gmail.com>
2019-09-12 22:07:31 +0000

    fixed pep8

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,11 +5,11 @@ if __name__ == "__main__":
     if a == 1:
         print("{:d} arguments.".format(a - 1))
     elif a == 2:
-        print("{:d} argument".format(a - 1))
+        print("{:d} argument:".format(a - 1))
         print("{:d}: {:s}".format(a - 1, sys.argv[1]))
     else:
         print("{:d} arguments".format(a - 1))
         for i in range(a):
             if i == 0:
                 continue
-            print("{:d} : {:s}".format(i, sys.argv[i]))
+            print("{:d}: {:s}".format(i, sys.argv[i]))
