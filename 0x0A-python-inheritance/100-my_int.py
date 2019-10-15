c4a4da9180fc2a75ea82c782651ab9f33b7b462a
nataliamedinat <nataliamedinabotero@gmail.com>
2019-10-15 23:29:30 +0000

    Operators inverted

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+class MyInt(int):
+    def __eq__(self, other):
+        return (int(self) != int(other))
+
+    def __ne__(self, other):
+        return (int(self) == int(other))
+
