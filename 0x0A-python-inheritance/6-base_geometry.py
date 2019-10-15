e38916200637032e52bd71b547a20d10ea8dc712
nataliamedinat <nataliamedinabotero@gmail.com>
2019-10-15 21:46:02 +0000

    Improve Geometry

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -3,4 +3,6 @@ class BaseGeometry:
     """Define a BaseGeometry class
     """
     def area(self):
+        """Public instance method
+        """
         raise Exception("area() is not implemented")
