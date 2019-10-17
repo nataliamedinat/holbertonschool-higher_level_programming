3b7013ae64371395e69b4d76701920136cc8909e
nataliamedinat <nataliamedinabotero@gmail.com>
2019-10-17 04:56:24 +0000

    Class to json

---
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Function that returns the dictionary description with
+   simple data structure (list, dictionary, string,
+   integer and boolean) for JSON serialization of an object:
+"""
+
+
+def class_to_json(obj):
+    """Return a dictonary description with
+       simple data structure for json
+    """
+    return obj.__dict__
