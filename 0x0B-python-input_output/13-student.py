9360376b34e7f408f526f1f7d37b8300270d2682
nataliamedinat <nataliamedinabotero@gmail.com>
2019-10-17 05:21:04 +0000

    kind of

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+class Student:
+    """Class that defines a student
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if type(attrs) == str:
+
+        return self.__dict__
+ 
