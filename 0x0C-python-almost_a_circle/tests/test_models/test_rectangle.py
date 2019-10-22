1f11b07fd7280678e53399021938474e471fc673
nataliamedinat <nataliamedinabotero@gmail.com>
2019-10-22 21:25:34 +0000

    Test cases

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Module to test rectangle class """
+import sys
+import unittest
+from models.rectangle import Rectangle
+
+
+class TestRectangle(unittest.TestCase):
+    """ Class for test cases for the rectangle class """
