d698cdd720edc78656601bb1c302fc672baeb8a6
nataliamedinat <nataliamedinabotero@gmail.com>
2019-10-22 23:25:24 +0000

    Disctionary to json string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Module for Base class
 """
+import json
 
 
 class Base:
@@ -16,3 +17,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """ Returns the json representation """
+        if list_dictionaries is None:
+            return("[]")
+        else:
+            return json.dumps(list_dictionaries)
