3d0dfe15772f3431f7538ee663b269d75bc8209e
nataliamedinat <nataliamedinabotero@gmail.com>
2019-10-22 21:38:23 +0000

    Square, first part

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Module for square class """
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Class square inherit from Rectangle
+    """
+    def __init__(self, size, x=0, y=0, id=None):
+        """ Constructor """
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        """Return a string
+        """
+        return ("[Square] ({}) {}/{} - {}".format(self.id, self.__x,
+                self.__y, self.width))
