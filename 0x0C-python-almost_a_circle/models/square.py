526e8f62afa0ab54e4827ae434bdf6f993c101f3
nataliamedinat <nataliamedinabotero@gmail.com>
2019-10-22 22:46:01 +0000

    Square update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -25,3 +25,25 @@ class Square(Rectangle):
         """
         return ("[Square] ({}) {}/{} - {}".format(self.id, self.x,
                 self.y, self.width))
+
+    def __update(self, id=None, size=None, x=None, y=None):
+        """ Internar method that updates attributes """
+        if id is not None:
+            self.id = id
+
+        if size is not None:
+            self.size = size
+
+        if x is not None:
+            self.x = x
+
+        if y is not None:
+            self.y = y
+
+    def update(self, *args, **kwargs):
+        """Assigns attributes """
+        if args:
+            self.__update(*args)
+
+        if kwargs:
+            self.__update(**kwargs)
