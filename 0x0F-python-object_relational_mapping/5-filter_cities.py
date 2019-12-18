ba7efb4f3bf6c6376ccfda0ec7b613e9946517b9
nataliamedinat <nataliamedinabotero@gmail.com>
2019-12-18 05:34:51 +0000

    All cities by state

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+# Lists all states from the database
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # Connection
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3])
+    # Executing queries
+    cur = db.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities\
+    INNER JOIN states ON cities.state_id = states.id\
+    WHERE states.name = %s\
+    ORDER BY cities.id ASC", (argv[4], ))
+
+    # To obtain results
+    cities = ''
+    res = cur.fetchall()
+    for row in res:
+        cities.append(row[1])
+        print(", ".join(cities))
+
+    # Clean up
+    cur.close()
+    db.close()
