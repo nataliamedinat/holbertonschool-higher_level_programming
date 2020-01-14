a1321fa109851908006004ea665102b3ea62b404
nataliamedinat <nataliamedinabotero@gmail.com>
2020-01-14 04:26:02 +0000

    Sends a request to the URL and displays the body, mage HTTPERROR

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-import urllib.request
-import urllib
 import sys
+import urllib
+import urllib.request
 
 if __name__ == "__main__":
     url = sys.argv[1]
     try:
         with urllib.request.urlopen(url) as response:
             read = response.read()
-            print(read.decode('utf-8'))
-    except urllib.error.HTTPERROR as error:
+            print(read.decode("UTF-8"))
+    except urllib.error.HTTPError as error:
         print("Error code: {}".format(error.code))
