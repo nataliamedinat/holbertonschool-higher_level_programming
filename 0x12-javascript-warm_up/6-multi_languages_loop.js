efdf330d461d6d0300513209c5637a8e4a230c68
nataliamedinat <nataliamedinabotero@gmail.com>
2020-02-04 23:46:29 +0000

    Loop to an array

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+
+const languages = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let trav = 0; trav < languages.length; trav++) {
+  console.log(languages[trav]);
+}
