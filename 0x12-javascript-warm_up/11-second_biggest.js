bcd8137c5880259d4934664f310a4e5e9279fa7f
nataliamedinat <nataliamedinabotero@gmail.com>
2020-02-05 04:23:20 +0000

    Fixed semistandard

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -10,9 +10,9 @@ if (argv.length <= 3) {
   console.log(0);
 } else {
   const array = argv.slice(2);
-  const m_array = array.map(Number);
-  const filt = m_array.filter(filtr);
-  const array_sort = filt.sort(function (a, b) { return a - b; });
-  const final_arr = array_sort[array_sort.length - 2];
-  console.log(final_arr);
+  const ConvertArr = array.map(Number);
+  const filt = ConvertArr.filter(filtr);
+  const arraySort = filt.sort(function (a, b) { return a - b; });
+  const FinalArr = arraySort[arraySort.length - 2];
+  console.log(FinalArr);
 }
