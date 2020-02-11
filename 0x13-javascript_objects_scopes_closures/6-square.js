e2f95d1012fdc57c6e8c926ad6796fd638f39c86
nataliamedinat <nataliamedinabotero@gmail.com>
2020-02-11 04:47:05 +0000

    Added a new method

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-const Rectangle = require('./4-rectangle');
+const Rectangle = require('./5-square');
 
-module.exports = class Square extends Rectangle {
+module.exports = class Square extends Square1 {
   constructor (size) {
     super(size, size);
   }
