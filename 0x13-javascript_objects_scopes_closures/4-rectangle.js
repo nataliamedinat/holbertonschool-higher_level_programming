5922da1c3daa930073041b7c6e06e163868bfbaf
nataliamedinat <nataliamedinabotero@gmail.com>
2020-02-11 04:14:59 +0000

    Semistandard fixed

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -14,14 +14,13 @@ module.exports = class Rectangle {
     }
   }
 
-  rotate() {
-  this.width = this.height;
-  this.height = this.width;
+  rotate () {
+    this.width = this.height;
+    this.height = this.width;
   }
 
-
-  double() {
-  this.width *= 2;
-  this.height *= 2;
-}
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
 };
